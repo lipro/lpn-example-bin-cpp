5d0f15c10db07fdcd98c9a8c27336cabea21a607
Stephan Linz <linz@li-pro.net>
2018-08-20 10:29:50 +0200

    cmake: initial project setup
    Signed-off-by: Stephan Linz <linz@li-pro.net>

---
+++ src/lpn_example.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "lpn_example_bin_cpp.h"
+
+int main(int argc, char *argv[]){
+	std::cout << "LPN example in C++!" << std::endl;
+	std::cout << "BIN v" << lpn_example_bin_cpp_VERSION_MAJOR; 
+	std::cout << "." << lpn_example_bin_cpp_VERSION_MINOR << std::endl; 
+	return 0;
+}
