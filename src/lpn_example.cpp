a17388fa1e1ef1ae5d6a9f92db06e88628a8b919
Stephan Linz <linz@li-pro.net>
2018-08-27 20:52:39 +0200

    fix: ignore unused arguments
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- src/lpn_example.cpp
+++ src/lpn_example.cpp
@@ -2,6 +2,8 @@
 #include "lpn_example_bin_cpp.h"
 
 int main(int argc, char *argv[]){
+	(void) argc;	/* unused */
+	(void) argv;	/* unused */
 	std::cout << "LPN example in C++!" << std::endl;
 	std::cout << "BIN v" << lpn_example_bin_cpp_VERSION_MAJOR; 
 	std::cout << "." << lpn_example_bin_cpp_VERSION_MINOR << std::endl; 
